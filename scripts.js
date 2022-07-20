0e18ac3657dea6c94c49c19cc8b128f076db329a
ezequielh14 <ezequielim44@gmail.com>
2022-07-20 08:29:22 -0300

    :sparkles: Feat: starting with JS

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const name = "Ezequiel Hornos";
+let age = 24;
+let isCouple = false;
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const skillers = {
+  name_skiller_1: "Ian Pereyra",
+  name_skiller_2: "Guardini Philama",
+  name_skiller_3: "Jesica Alfaro",
+  name_skiller_4: "Melina Lencina",
+  name_skiller_5: "Gonzalo Herrero",
+};
+
+const objectJS = {
+  name: "Ezequiel Hornos",
+  age: 24,
+  isCouple: false,
+  nums: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  name_skiller_1: "Ian Pereyra",
+  name_skiller_2: "Guardini Philama",
+  name_skiller_3: "Jesica Alfaro",
+  name_skiller_4: "Melina Lencina",
+  name_skiller_5: "Gonzalo Herrero",
+};
+
+console.log(name);
+console.log(age);
+console.log(isCouple);
+console.log(nums);
+console.log(skillers);
+console.log(objectJS);
