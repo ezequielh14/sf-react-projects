139ecb3a12a9fd730fce6da83e0dab30714f803d
ezequielh14 <ezequielim44@gmail.com>
2022-08-13 03:52:45 -0300

    :bug: Fix: New changes

--- js/rickmortyAPI.js
+++ js/rickmortyAPI.js
@@ -1,30 +1,55 @@
-const characterList = async () => {
-  
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-    const characters = await response.json();
-    console.log(characters);
+const spinner = document.querySelector("#spinner");
+const nav_pagination = document.getElementById("nav-pagination");
+const url = 'https://rickandmortyapi.com/api/character';
+const characterList = async (url) => {
 
-let cardsBody = ``;
-characters.forEach((character, index) => {
+  const response = await fetch(url);
+  const characters = await response.json();
+  spinner.style.display = "none";
+  paginationCharacters(characters.info);
+  console.log(characters);
+  
+  let cardsBody = ``;
+  characters.results.forEach((character, index)=>{
     console.log(character);
-    cardsBody += `<div class="col mt-5">
-    <div class="card" style="width: 13rem;">
-    <img src="${character.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">${character.name}</h5>
-    <p class="card-text">Gender: ${character.gender}</p>
-    <p class="card-text">Species: ${character.species}</p>
+    cardsBody += `<div class="flip-card">
+    <div class="card-container">
+    <div class="character-block">
+    <div class="img-container">
+    <img src="${character.image}" alt="${character.name}">
+    </div>
+    <p>#${character.id}</p>
+    <p class="name">${character.name}</p>
+    </div>
+    <div class="character-block-back">
+    <div class="info-container">
+    <p>Origin: ${character.origin.name}</p>
+    <p>Species: ${character.species}</p>
+    <p>Gender: ${character.gender}</p>
+    </div>
     </div>
     </div>
     </div>`;
-});
-document.getElementById("tableUsers").innerHTML = cardsBody;
+  });
+  document.getElementById("cardsBody").innerHTML = cardsBody;
 }
 
-const paginationCharacters = () => {
-    let paginationBody = `<li class="page-item"><span class="page-link">Previous</span></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>`;
-    document.getElementById("pagination").innerHTML = paginationBody;
+const paginationCharacters = (info) => {
+  
+  let preDisable = info.prev == null ? 'disabled' : '';
+  let nextDisable = info.next == null ? 'disabled' : '';
+    let paginationBody = ``;
+  paginationBody += `
+  <ul class="pagination">
+        <li class="page-item ${preDisable}" id="previous">
+          <a class="page-link" href="#" tabindex="-1" onClick="characterList('${info.prev}')">Previous</a>
+        </li>
+        <li class="page-item ${nextDisable}" id="next">
+          <a class="page-link" href="#" onClick="characterList('${info.next}')">Next</a>
+        </li>
+      </ul>`;
+
+      nav_pagination.innerHTML = paginationBody;
 }
 
-listUsers();
+characterList(url);
